fd28777b3053f62a66b29d99d30161fb672706cb
Nurillo1997 <nurilloabdurazzoqov@gmail.com>
2023-05-13 05:29:04 +0900

    fix: refactor

--- server.js
+++ server.js
@@ -36,22 +36,22 @@ app.set("views", "views");
 app.set("view engine", "ejs");//ejs orqali backentda frontend yasaladi
 //4:Router code
 //router hosil qilindi
-app.get("/hello", function (reg, res) {
-    res.end(`<h1 style="background: red ">hello world by Nurillo</h1>`);
-});
-
-app.get("/gift", function (reg, res) {
-    res.end(`<h1>Siz sovg'alar bo'limidasiz</h1>`);
-});
-//formni actioni ga taluqli
-// app.post("/create-item", (req, res) => {
-//     console.log(req.body);
-//     res.json({ test: "success" });
+// app.get("/hello", function (reg, res) {
+//     res.end(`<h1 style="background: red ">hello world by Nurillo</h1>`);
 // });
 
-// app.get("/", function (req, res) {
-//     res.render(`harid`);
+// app.get("/gift", function (reg, res) {
+//     res.end(`<h1>Siz sovg'alar bo'limidasiz</h1>`);
 // });
+// formni actioni ga taluqli
+app.post("/create-item", (req, res) => {
+    // console.log(req.body);
+    // res.json({ test: "success" });
+});
+
+app.get("/", function (req, res) {
+    res.render(`reja`);
+});
 app.get("/author", (req, res) => {
     res.render(`author`, { user: user });
 });
@@ -60,5 +60,5 @@ app.get("/author", (req, res) => {
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function () {
-    console.log(`The server is running successfuly on port ${PORT}`);
+    console.log(`The server is running successfuly on port ${PORT}, http://localhost:${PORT}`);
 });
