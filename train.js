0de0bfd106f3ceb2a32e814616bbdf15e5904021
Nurillo1997 <nurilloabdurazzoqov@gmail.com>
2023-05-25 01:57:45 +0900

    feat: task-E tub sonni topish

--- train.js
+++ train.js
@@ -1,47 +1,17 @@
-
-
-class Shop {
-    constructor(non, lagmon, cola) {
-        this.non = non;
-        this.lagmon = lagmon;
-        this.cola = cola;
-        this.products = { non, lagmon, cola };
+//task-E tub sonni topish
+function tubSonmi(num) {
+    if (num <= 1) {
+      return false;
     }
-
-    getTime() {
-        const date = new Date();
-        return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  
+    for (let i = 2; i < num; i++) {
+      if (num % i === 0) {
+        return false;
+      }
     }
-
-    log(message) {
-        console.log(`${message}`);
-    }
-
-    qoldiq() {
-        const message = `hozir ${this.getTime()}da ${this.non}ta non, ${this.lagmon}ta lagmon va ${this.cola}ta cola mavjud!`;
-        this.log(message);
-        return message;
-    }
-
-    sotish(product, quantity) {
-        if (this.products[product] >= quantity) {
-            this.products[product] -= quantity;
-            this[product] -= quantity;
-            this.log(`${quantity}ta ${product} sotildi, qoldiq: ${this.products[product]}`);
-        } else {
-            this.log(`Kechirasiz, ${product} yetarli miqdorda mavjud emas!`);
-        }
-    }
-
-    qabul(product, quantity) {
-        this.products[product] += quantity;
-        this[product] += quantity;
-        this.log(`${quantity}ta ${product} qabul qilindi, hozirgi qoldiq: ${this.products[product]}`);
-    }
-}
-
-const shop = new Shop(4, 5, 2);
-shop.qoldiq();
-shop.sotish('non', 2);
-shop.qabul('cola', 4);
-shop.qoldiq();
+  
+    return true;
+  }
+  
+  const test = tubSonmi(10);
+  console.log(test);
